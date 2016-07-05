fff1fccb4a80f66129f01b87fd95a651c1c017fd
7ictor <victor@rodriguezv.com>
2016-07-05 17:48:55 -0600

    Commenting, refactor useless stuff, get rid of semicolons

--- index.js
+++ index.js
@@ -1,82 +1,90 @@
-'use strict';
+'use strict'
 
-var couchapp = require('couchapp');
-var through = require('through2');
-var gutil = require('gulp-util');
-var path = require('path');
+module.exports.push = pushCouchapp
 
-var PLUGIN_NAME = 'gulp-couchapp';
-var PluginError = gutil.PluginError;
+var couchapp = require('couchapp')
+var through = require('through2')
+var gutil = require('gulp-util')
+var path = require('path')
+var PluginError = gutil.PluginError
 
+const PLUGIN_NAME = 'gulp-couchapp'
+
+/**
+ * Determine whether an object has the specified property as a direct property.
+ *
+ * @param  {Object}  object
+ * @param  {String}  key
+ * @return {Boolean}
+ */
 function has(object, key) {
-  return Object.prototype.hasOwnProperty.call(object, key);
+  return Object.prototype.hasOwnProperty.call(object, key)
 }
 
-function buildURL(db, opts) {
-  var url = 'http://';
-
-  // Set defaults.
-  if (!has(opts, 'host')) opts.host = '127.0.0.1';
-  if (!has(opts, 'port')) opts.port = '5984';
+/**
+ * Build the URL to push CouchApp.
+ *
+ * @param  {String} dbName
+ * @param  {Object} opts
+ * @return {String}
+ */
+function buildURL(dbName, opts) {
+  var authentication
+  opts.scheme = opts.scheme || 'http'
+  opts.host   = opts.host   || '127.0.0.1'
+  opts.port   = opts.port   || '5984'
 
   if (has(opts, 'auth')) {
     if (typeof opts.auth === 'object') {
-      url += opts.auth.username + ':' + opts.auth.password + '@';
+      authentication = opts.auth.username + ':' + opts.auth.password + '@'
     }
   }
 
-  url += opts.host + ':' + opts.port + '/' + db;
-
-  return url;
+  return opts.scheme + '://' + authentication + opts.host + ':' + opts.port + '/' + dbName
 }
 
-function couchappPlugin(db, opts) {
-  if (!db && typeof db !== 'string') {
+/**
+ * Push a CouchApp to a CouchDB database.
+ *
+ * @param  {String} db
+ * @param  {Object} opts
+ * @return {Stream}
+ */
+function pushCouchapp(dbName, opts) {
+  opts = opts || {}
+
+  if (!dbName && typeof dbName !== 'string') {
     throw new PluginError(PLUGIN_NAME, 'Missing database name.');
   }
 
-  opts = opts || {};
-  var ddocObj,
-      url;
-
-  var stream =  through.obj(function(file, enc, cb) {
-    var error;
+  return through.obj(function (file, enc, cb) {
+    var ddocObj = require(file.path)
+    var url = buildURL(dbName, opts);
 
     if (file.isNull()) {
-      return cb(error, file);
+      return cb(null, file)
     }
 
     if (file.isStream()) {
-      error = new PluginError(PLUGIN_NAME, 'Streaming not supported.');
-      return cb(error, file);
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported.'))
+      return cb(null, file)
     }
 
     if (path.extname(file.path) !== '.js') {
-      error = new PluginError(PLUGIN_NAME, 'File extension not supported.');
-      return cb(error, file);
+      this.emit('error', new PluginError(PLUGIN_NAME, 'File extension not supported.'))
+      return cb(null, file)
     }
 
-    ddocObj = require(file.path);
     if (has(opts, 'attachments')) {
-      // Load attachments to ddoc.
-      couchapp.loadAttachments(
-        ddocObj,
-        path.join(process.cwd(), path.normalize(opts.attachments))
-      );
+      var attachmentsPath = path.join(process.cwd(), path.normalize(opts.attachments))
+      couchapp.loadAttachments(ddocObj, attachmentsPath)
     }
-
-    url = buildURL(db, opts);
 
     couchapp.createApp(ddocObj, url, function (app) {
       app.push(function () {
-        gutil.log(PLUGIN_NAME, 'Couchapp pushed!');
-        return cb(null, file);
-      });
-    });
-
-  });
-
-  return stream;
+        gutil.log(PLUGIN_NAME, 'Couchapp pushed!')
+        return cb(null, file)
+      })
+    })
+  })
 }
-
-module.exports = { push : couchappPlugin };
